07b43f832097ca38a10c3e79a912c869da5f57c1
Joshua Duryea <duryeajoshua@gmail.com>
2022-09-30 13:03:10 -0500

    quiz questions added

---
+++ main.js
@@ -0,0 +1,38 @@
+const quizData = [
+  {
+    question: 'In what year were the first Air Jordan sneakers released?',
+    a: '1984',
+    b: '1989',
+    c: '1996',
+    d: '1999',
+    correct: 'a' 
+  }, {
+    question: 'which country consumes the most chocolate per capita?',
+    a: 'United States',
+    b: 'Switzerland',
+    c: 'Germany',
+    d: 'Loompaland',
+    correct: 'b' 
+  }, {
+    question: 'wich 2 U.S. states do not observe Daylight Savings Time?',
+    a: 'Arizona and Hawaii',
+    b: 'Florida and Texas',
+    c: 'Alaska and Main',
+    d: 'Washing D.C. and Puerto Rico',
+    correct: 'a' 
+  }, {
+    question: 'What is the loudest animal on Earch?',
+    a: 'White Elephant',
+    b: 'Lion',
+    c: 'Sperm whale',
+    d: 'Mandrake',
+    correct: 'c' 
+  }, {
+    question: 'What are baby rabbits called?',
+    a: 'Rats',
+    b: 'Bits',
+    c: 'Kits',
+    d: 'Ribs',
+    correct: 'c' 
+  },
+]
